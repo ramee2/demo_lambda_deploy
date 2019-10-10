27ae0bc4f2b843bbbc3ae2f34dfb580739b82817
ramee2 <rizi.ameer15@gmail.com>
2019-10-10 13:55:51 -0500

    first commit

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     print("Hello from Lambda Function") 
     return { 
         'statusCode': 200, 
-        'body': 'Hello from Lambda Function' 
+        'body': 'Hello mere bhai' 
     } 
