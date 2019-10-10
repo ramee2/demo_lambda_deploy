4da01f880757763c8fcf054bfa63b21674078c6d
Rizwan Ameer <37174630+ramee2@users.noreply.github.com>
2019-10-10 07:12:47 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     print("Hello from Lambda Function") 
     return { 
         'statusCode': 200, 
-        'body': 'Hello Rizwan, from Lambda Function :)' 
+        'body': 'Hello Rizwan, from Lambda Function' 
     } 
